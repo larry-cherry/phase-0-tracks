fa5876fc2effe205f3ef5e2a38c7ed56fe2f32f9
Larry D. Cherry <larry.cherry.0406@gmail.com>
2016-11-16 16:12:10 -0600

    modified user input to proper data type

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,15 +1,29 @@
-puts "What is your Hamsters name?"
+#Ask user for hampster name
+puts "What is your Hamsters name?" 
 name = gets.chomp
+
+#Ask user how squeaky the hampster is?
 puts "How squeaky is your Hampster? 1 being quite and 10 being very squeaky"
-squeaky = gets.chomp
+squeaky = gets.chomp.to_i
+
+#Ask user what color the hampster is?
 puts "What color fur does your Hampster have?"
 fur = gets.chomp
+
+#Ask user if the hampster is good for adoption
 puts "Is your hampster a good canidate for adoption (y/n)"
 adoption = gets.chomp.upcase
+	if adoption == "Y"
+		adoption = true
+	else 
+		adoption = false
+	end
+
+#Ask for estimated age of hampster
 puts "What is the estimated age of your hampster?"
 age = gets.chomp
 	if  age == ""
 		age = nil
-		
+	else
+		age.to_f
 	end
-p age
