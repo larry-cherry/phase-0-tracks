94e7f0204c5b44c22dba3635cf827854f77fc822
Larry D. Cherry <larry.cherry.0406@gmail.com>
2016-11-24 21:17:48 -0600

    Wrote some Pseudocode for the program and created a few essentail varaibles

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,17 @@
+
+#Ask spy for their real name
+#Swap the first name and last name
+#Find the number of characters in the name
+#Chnage the vowels to the next letter in the (aeiou) string
+#Change the Consonants to the next consonant in the alphabet. 
+#Print the results
+vowels = "aeiou"
+consonants = "bcdfghjklmpqrstuvwxyz"
+
+puts "What is your name?"
+real_name = gets.chomp.to_s
+
+p real_name
+p real_name.length
+p vowels
+p consonants
