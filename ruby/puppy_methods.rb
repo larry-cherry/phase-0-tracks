e835bc5ad82f3eac3f7783174878b761df43dd19
Larry D. Cherry <larry.cherry.0406@gmail.com>
2016-11-29 20:17:08 -0600

    Worked with James on 6.2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,86 @@
+
 class Puppy
+
+	def initialize
+		puts "Initializing new puppy instance ..."
+	end
 
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
 
+  def speak(times)
+  	puts "Whoof" * times
+  end
+
+  def roll_over
+  	puts "*rolls over*"
+  end
+
+  def dog_years(years)
+
+  	puts "Your dog is #{years*7} years old in dog years."
+
+  end
+
+  def eats_socks(number)
+
+  	puts "Your dog ate #{number} socks!"
+  end
+
+
+
+
 end
 
+dex = Puppy.new
+	
+dex.fetch("birds")
+dex.speak(3)
+dex.roll_over
+dex.dog_years(2)
+dex.eats_socks(3)
+
+class Beer
+
+ def initialize
+   puts "Pouring you a beer..."
+ end
+
+ def style
+   type = ["IPA", "Stout", "Lager", "Pale Ale"]
+   beer_style = type.sample
+   if beer_style == "IPA"
+     puts "You are drinking an #{beer_style} style beer!"
+   else
+     puts "You are drinking a #{beer_style} style beer!"
+   end
+ end
+
+ def drink(sips)
+   if sips == 1
+     puts "You took #{sips} sip."
+   else
+     puts "You took #{sips} sips."
+   end
+ end
+
+end
+
+# Create empty array to store Beer instances
+all_beers = []
+
+# Create 50 instances and store in array
+50.times { all_beers.push(Beer.new) }
+
+# Loop through beer array
+ # Prints how each style of beer
+ # Prints how many sips you took randomly from 1-10
+
+all_beers.each do |beer|
+ puts beer.style
+ puts beer.drink(rand(1..10))
+end
+
+puts all_beers.length
