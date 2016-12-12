8f1e00c5d9a5a37acc59abc22371fa84d1122704
Larry D. Cherry <larry.cherry.0406@gmail.com>
2016-12-12 04:50:40 -0600

    Made a mistake

--- js/explore.js
+++ js/explore.js
@@ -13,20 +13,22 @@
 //newString = newString.split(",");
 //console.log(newString);
 
-//function Reverse(word) {
-	
-	//console.log("Your current word is", word);
-	
-	//this.make = make;
-	//this.model = model;
-	//this.year = year;
-	//this.color = color;
-	
-	//this.honk = function() { console.log("Honk Honk"); };
-//}
-//created a function that turns a string into an array, reverses the array, and then turns it back into a string
+
+function Reverse(word) {
+
+	this.word = word;
+	//console.log(word) 
+	word = word.split("");
+	//console.log(word);
+	word = word.reverse();
+	//console.log(word);
+	word = word.join('');
+	//console.log(word);
+	this.nword = word
 
 
+
+}
 var word = "hello";
 var newword1 = new Reverse(word);
 console.log(newword1.nword)
